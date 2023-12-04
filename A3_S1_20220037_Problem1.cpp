a17de0bac4b5b1cf63a470744a28f2b1dd6fa6c1
elkholyjr <119688248+elkholyjr@users.noreply.github.com>
2023-12-04 14:21:54 +0200

    Create A3_S1_20220037_Problem1.cpp

---
+++ A3_S1_20220037_Problem1.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <fstream>
+class LabelGenerator{
+protected:
+    string figure;
+    int Index;
+public:
+
+LabelGenerator(string s,int x) : figure(s),Index(x) {}
+
+virtual string nextLabel(){
+    return figure+ to_string(Index++);
+}
+
+};
+
+class FileLabelGenerator : public LabelGenerator{
+private:
+    ifstream file;
+public:
+
+    FileLabelGenerator(string s,int x,string filename) : LabelGenerator(s,x) , file(filename){}
+
+    string nextLabel() override{
+        string line;
+        if(getline(file,line)){
+            return LabelGenerator:: nextLabel() + " " + line;
+        }else{
+            return LabelGenerator::  nextLabel();
+        }
+    }
+
+};
+
+int main() {
+    FileLabelGenerator F1 ("Figure",1,"Test.txt");
+
+    cout<<"File :\n";
+
+    for(int i=0;i<5;i++){
+        (i==2) ? cout<<F1.nextLabel(): cout<<F1.nextLabel()<<',';
+    }
+
+    cout<<"\n";
+
+    LabelGenerator figureNumbers("Figure ", 1);
+    LabelGenerator pointNumbers("P", 0);
+
+    cout << "Figure numbers: ";
+    for (int i = 0; i < 5; i++) {
+        (i==2) ? cout << figureNumbers.nextLabel() : cout << figureNumbers.nextLabel() << ", " ;
+    }
+
+    cout << "\nPoint numbers: ";
+    for (int i = 0; i < 5; i++) {
+        (i==4) ? cout << pointNumbers.nextLabel() : cout << pointNumbers.nextLabel() << ", ";
+    }
+
+    cout << "\nMore figures: ";
+    for (int i = 0; i < 3; i++) {
+        (i==2) ? cout << figureNumbers.nextLabel() : cout << figureNumbers.nextLabel() << ", ";
+    }
+
+    cout << endl;
+    return 0;
+}
