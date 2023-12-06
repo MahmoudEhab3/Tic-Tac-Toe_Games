bed6c6285ba52d57ade4291844b5d76ce405903f
elkholyjr <119688248+elkholyjr@users.noreply.github.com>
2023-12-06 18:09:33 +0200

    Update RandomPlayer.cpp

--- Task 2/Game_1/RandomPlayer.cpp
+++ Task 2/Game_1/RandomPlayer.cpp
@@ -19,11 +19,44 @@ RandomPlayer::RandomPlayer (char symbol, int dimension):Player(symbol)
 }
 
 // Generate a random move
-void RandomPlayer::get_move (int& x, int& y) {
+void RandomPlayer::get_move (int& x, int& y)  {
+
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3 + i; ++j) {
+            if (j >= 3 - 1 - i && j <= 3 - 1 + i) {
+                if (board[i][j] == 0) {
+                    board[i][j] = 'O';
+                    if (is_winner()) {
+                        x = i, y = j;
+                        return;
+                    }
+                    board[i][j] = ' '; // Undo the move
+                }
+            }
+        }
+    }
+
+    // Check if the human player is about to win and block that move
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3 + i; ++j) {
+            if (j >= 3 - 1 - i && j <= 3 - 1 + i) {
+                if (board[i][j] == 0) {
+                    board[i][j] = 'X';
+                    if (is_winner()) {
+                        x = i, y = j;
+                        return;
+                    }
+                    board[i][j] = ' '; // Undo the move
+                }
+            }
+        }
+    }
+
     x = (int) (rand()/(RAND_MAX + 1.0) * dimension);
     y = (int) (rand()/(RAND_MAX + 1.0) * dimension);
     while((x==0&&y==0)||(x==0&&y==1)||(x==0&&y==3)||(x==0&&y==4)||(x==1&&y==0)||(x==1&&y==4)){
         x = (int) (rand()/(RAND_MAX + 1.0) * dimension);
         y = (int) (rand()/(RAND_MAX + 1.0) * dimension);
     }
+
 }
