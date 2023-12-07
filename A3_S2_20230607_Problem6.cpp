5864f2e47edf2ea182aff6e67b328f66a03d102d
Mazen-Abdelfattah <147278273+Mazen-Abdelfattah@users.noreply.github.com>
2023-12-07 22:35:12 +0200

    Update A3_S2_20230607_Problem6.cpp

--- A3_S2_20230607_Problem6.cpp
+++ A3_S2_20230607_Problem6.cpp
@@ -11,12 +11,14 @@ using namespace std;
 class Universe{
 private:
     int rows,columns;
-    vector<vector<bool> > grid;
+    vector<vector<bool> > grid; //2D vector of booleans
 public:
-    Universe(int rows,int columns) : rows(rows),columns(columns){
-        grid.resize(rows, vector<bool>(columns, false));
+    Universe(int rows,int columns) : rows(rows),columns(columns){ //Parameterized constructor
+        grid.resize(rows, vector<bool>(columns, false) );
     }
-    void initialize() {
+    void initialize() { //to initialize the cells
+        //We will use 'srand' to randomly choose the places of the cells
+        //The srand and rand functions in C++ are used for generating pseudo-random numbers.
         srand(time(nullptr));
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < columns; ++j) {
@@ -24,22 +26,22 @@ public:
             }
         }
     }
-    void resest(){
+    void resest(){ //Set all cells as dead
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < columns; ++j) {
              grid[i][j] = false;
             }
         }
     }
-    int count_neighbors(int row,int col){
-        int count = 0;
-        for (int i = -1; i <= 1; ++i) {
+    int count_neighbors(int row,int col){//To return the number of alive cells around SPECIFIC cell
+        int count = 0;                   //We use it in 'next generation' function
+        for (int i = -1; i <= 1; ++i) { //We set the 2 loops -1 0 1 to iterate over the 3*3 square around the cell
             for (int j = -1; j <= 1; ++j) {
                 if (i == 0 && j == 0) {
                     continue;  // Skip the current cell
                 }
-                int neighborRow = (row + i + rows) % rows;
-                int neighborColumn = (col + j + columns) % columns;
+                int neighborRow = (row + i + rows) % rows;          //toroidal (or wrapping) behavior.
+                int neighborColumn = (col + j + columns) % columns; //toroidal (or wrapping) behavior.
                 if (grid[neighborRow][neighborColumn]) {
                     count++;
                 }
@@ -52,6 +54,7 @@ public:
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < columns; ++j) {
                 int aliveNeighbors = count_neighbors(i, j);
+                //Using count_neighbors function to know the alive neighbor cells
                 if(grid[i][j]){
                     if(aliveNeighbors < 2 || aliveNeighbors > 3){
                         nextGrid[i][j] = false;
@@ -69,19 +72,18 @@ public:
     void display(){
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < columns; ++j) {
-                if (grid[i][j]) {
-                    std::cout << 'O';
+                if (grid[i][j]) { 
+                    cout << '*';
                 } else {
-                    std::cout << '.';
+                    cout << '.';
                 }
             }
-            std::cout << '\n';
+            cout << '\n';
         }
-        std::cout << '\n';
+        cout << '\n';
     }
     void run(int iterations){
         initialize();
-
         for (int i = 0; i < iterations; ++i) {
             display();
             next_generation();
@@ -90,12 +92,10 @@ public:
 };
 
 int main() {
-
     int rows = 20;
-    int columns = 50;
-    int iterations = 2;
+    int columns = 20;
+    int iterations = 5;
 
     Universe universe(rows, columns);
     universe.run(iterations);
-
 }
