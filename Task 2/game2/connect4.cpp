c1feb3e2280a80c04b73ce43b21e149bca76730c
MahmoudEhab3 <138493314+MahmoudEhab3@users.noreply.github.com>
2023-12-12 21:35:12 +0200

    Update connect4.cpp

--- Task 2/game2/connect4.cpp
+++ Task 2/game2/connect4.cpp
@@ -1,3 +1,10 @@
+// File: A3_S1_20220457_.cpp
+// Purpose: Connect4
+// Author: Mahmoud Ehab
+// Section: S1
+// ID: 20220457
+// TA: Khaled Ibrahim
+
 #include "connect4.h"
 #include "BoardGame_Classes.hpp"
 using namespace std;
