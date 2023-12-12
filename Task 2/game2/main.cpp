b06d0f222e0f82769c497d2ef49e1ad85f448886
MahmoudEhab3 <138493314+MahmoudEhab3@users.noreply.github.com>
2023-12-12 21:35:39 +0200

    Update main.cpp

--- Task 2/game2/main.cpp
+++ Task 2/game2/main.cpp
@@ -1,3 +1,10 @@
+// File: A3_S1_20220457_.cpp
+// Purpose: Connect4
+// Author: Mahmoud Ehab
+// Section: S1
+// ID: 20220457
+// TA: Khaled Ibrahim
+
 #include <iostream>
 #include <bits/stdc++.h>
 #include "connect4.h"
