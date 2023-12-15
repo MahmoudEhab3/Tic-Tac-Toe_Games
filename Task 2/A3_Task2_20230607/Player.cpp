3ca08e6817de20f597830229f5dc6b3519fc4296
Mazen-Abdelfattah <147278273+Mazen-Abdelfattah@users.noreply.github.com>
2023-12-15 19:29:39 +0200

    Update Player.cpp

--- Task 2/A3_Task2_20230607/Player.cpp
+++ Task 2/A3_Task2_20230607/Player.cpp
@@ -24,10 +24,10 @@ Player::Player (int order, char symbol) {
     this->symbol = symbol;
 }
 
-// Get desired move: x y (each between 0 and 2)
+// Get desired move: x y (each between 0 and 4)
 // Virtual (can change for other player types)
 void Player::get_move (int& x, int& y) {
-    cout << "\nPlease enter your move x and y (0 to 2) separated by spaces: ";
+    cout << "\nPlease enter your move x and y (0 to 4) separated by spaces: ";
     cin >> x >> y;
 }
 
